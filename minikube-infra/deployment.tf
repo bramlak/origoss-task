5274714f5399918b72f382c628cba0882ac0deab
bramlak <kalmarbalint97@gmail.com>
2025-12-04 21:15:51 +0100

    feat(tf): convert kubernetes deployment yaml to terraform hcl

---
+++ minikube-infra/deployment.tf
@@ -0,0 +1,68 @@
+resource "kubernetes_deployment" "origoss_task_server" {
+  metadata {
+    name      = "origoss-task-server"
+    namespace = kubernetes_namespace.origoss_task.metadata[0].name
+    labels = {
+      app = "origoss-task"
+    }
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        app = "origoss-task"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "origoss-task"
+        }
+      }
+
+      spec {
+        container {
+          image = var.image
+          name  = "server"
+          image_pull_policy = "Always"
+
+          port {
+            container_port = 8080
+          }
+
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "128Mi"
+            }
+            limits = {
+              cpu    = "300"
+              memory = "256Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+            }
+            initial_delay_seconds = 10
+            period_seconds        = 10
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+        }
+      }
+    }
+  }
+}
---
+++ minikube-infra/variables.tf
@@ -0,0 +1,5 @@
+variable "replicas" {
+  description = "Number of replicas of server pods"
+  type = number
+  default = 1
+}
