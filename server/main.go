e21a39c4a7f13202b21b465c57443a8148aa90a3
bramlak <kalmarbalint97@gmail.com>
2025-12-04 13:32:48 +0100

    feat(server): create main.go with root endpoint

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"io"
+	"net/http"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	var message string = "Hello, World!\n";
+	log.Println("root requested. Response: ", message)
+	io.WriteString(w, message)
+}
+
+func loadPortFromEnvFile() string {
+		err := godotenv.Load()
+    if err != nil {
+    	log.Fatal("Error loading .env file: ", err)
+	  }
+	port := os.Getenv("PORT")
+	return port
+}
+
+func main() {
+	var port string = loadPortFromEnvFile();
+	http.HandleFunc("/", getRoot)
+	address := net.JoinHostPort("", port)
+	log.Println("Server is running on port", port)
+	http.ListenAndServe(address, nil)
+}
