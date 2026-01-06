1ae843cf2838c312019c5b1c2671e8d9173798c5
bramlak <kalmarbalint97@gmail.com>
2026-01-06 14:46:01 +0100

    feat(webserver): transfor metrics to random numberoutput

--- server/main.go
+++ server/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"net"
 	"io"
+	"log"
+	"math/rand/v2"
+	"net"
 	"net/http"
 	"os"
-	"log"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"strconv"
+	// "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	var message string = "Hello, World!\n";
+	var message string = "Hello, World!\n"
 	log.Println("root requested. Response: ", message)
 	io.WriteString(w, message)
 }
@@ -25,24 +27,29 @@ func getReadiness(w http.ResponseWriter, r *http.Request) {
 	log.Println("Readiness check requested")
 }
 
+func getRandomNumber(w http.ResponseWriter, r *http.Request) {
+	var number int = rand.IntN(100) + 500
+	log.Println("Random number requested")
+	io.WriteString(w, strconv.Itoa(number))
 
-func loadPort() string {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-        log.Println("Using default port 8080")
-    }
-    return port
 }
 
-
+func loadPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Println("Using default port 8080")
+	}
+	return port
+}
 
 func main() {
-	var port string = loadPort();
+	var port string = loadPort()
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/healthz", getHealth)
 	http.HandleFunc("/readyz", getReadiness)
-	http.Handle("/metrics", promhttp.Handler())
+	// http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/metrics", getRandomNumber)
 	address := net.JoinHostPort("", port)
 	log.Println("Server is running on port", port)
 	http.ListenAndServe(address, nil)
